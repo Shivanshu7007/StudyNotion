9cd286f689bf94fc168147c295b45e67e2f12a64
Shivanshu Mishra <141329585+Shivanshu7007@users.noreply.github.com>
2023-09-09 22:06:29 +0530

    final commit 18

--- server/config/database.js
+++ server/config/database.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const { MONGODB_URL="mongodb+srv://shivanshumishra:8736955859@cluster0.hvbcah3.mongodb.net/Studynotion" } = process.env;
+const { MONGODB_URL } = process.env;
 
 exports.connect = () => {
 	mongoose
-		.connect("mongodb+srv://shivanshumishra:8736955859@cluster0.hvbcah3.mongodb.net/Studynotion", {
+		.connect(MONGODB_URL,{
 			useNewUrlparser: true,
 			useUnifiedTopology: true,
 		})
--- server/config/razorpay.js
+++ server/config/razorpay.js
@@ -1,12 +1,5 @@
-// const Razorpay = require("razorpay");
-
-// exports.instance = new Razorpay({
-// 	key_id: process.env.rzp_test_FCA1OEwVg0islf,
-// 	key_secret: 5PDUtHP6LyA1RZb0mORvGbZ9,
-// });
 const Razorpay = require('razorpay');
-
-const razorpay = new Razorpay({
-  key_id: 'rzp_test_FCA1OEwVg0islf',
-  key_secret: '5PDUtHP6LyA1RZb0mORvGbZ9',
+  exports.instance = new Razorpay({
+  key_id:process.env.RAZORPAY_KEY,
+  key_secret: RAZORPAY_SECRET,
 });
