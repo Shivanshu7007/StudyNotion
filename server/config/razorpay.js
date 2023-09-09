6174c64ede0ca14db3c4847bf3869a68d6fa0387
Shivanshu Mishra <141329585+Shivanshu7007@users.noreply.github.com>
2023-09-09 06:19:41 +0530

    final commit 6

--- server/config/razorpay.js
+++ server/config/razorpay.js
@@ -1,6 +1,6 @@
 const Razorpay = require("razorpay");
 
-exports.instance = new rzp_test_gZX227g9UcCxdC({
-	key_id: process.env.RAZORPAY_KEY,
+exports.instance = new Razorpay({
+	key_id: process.env.rzp_test_gZX227g9UcCxdC,
 	key_secret: process.env.RAZORPAY_SECRET,
 });
